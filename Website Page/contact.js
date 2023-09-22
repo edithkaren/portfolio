4e90147fa219575fe1d62ffa6c0fa4007730e9b3
Rudraksh Paliwal <75530058+edithkaren@users.noreply.github.com>
2023-09-22 17:16:48 +0530

    Add files via upload

---
+++ Website Page/contact.js
@@ -0,0 +1,12 @@
+
+var name = document.getElementById('Name'); 
+var email = document.getElementById('email');   
+var website = document.getElementById('website');
+var query = document.getElementById('query');
+
+
+function setdata(){
+
+    print("error");
+}
+
